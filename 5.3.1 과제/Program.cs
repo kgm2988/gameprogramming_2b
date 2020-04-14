7c44e8bd441f256c2ab972ca9e2871527e7238a1
kgm2988 <63137045+kgm2988@users.noreply.github.com>
2020-04-15 01:35:00 +0900

    겜플2B과제

---
+++ 5.3.1 과제/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Switch2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            object obj = null;
+
+            string s = Console.ReadLine();
+
+            if (int.TryParse(s, out int out_i))
+                obj = out_i;
+
+            else if (float.TryParse(s, out float out_f))
+                obj = out_f;
+
+            else obj = s;
+
+            
+            Console.WriteLine("{1}는 {0}형식입니다.",obj.GetType().Name,s);
+                    
+            
+
+        }
+    }
+}
